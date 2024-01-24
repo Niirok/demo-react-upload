e30e7a7fb949be807f96340726572d5c089de49d
Niirok <bouboulok@gmail.com>
2024-01-24 12:10:47 +0100

    First operational Dockerfile up

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,15 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+
   },
-})
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
+  }
+}
+)
